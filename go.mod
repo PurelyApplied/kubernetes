055797af93c6fb26a720e53cff70e328059c3a62
Patrick Rhomberg <patrhom@google.com>
2020-09-03 22:47:28 +0000

    Add levee script.  Must run locally or explicitly via make, due to Bazel environment / artifact sandboxing.

--- go.mod
+++ go.mod
@@ -55,6 +55,7 @@ require (
 	github.com/golang/mock v1.3.1
 	github.com/google/cadvisor v0.37.0
 	github.com/google/go-cmp v0.4.0
+	github.com/google/go-flow-levee v1.2
 	github.com/google/gofuzz v1.1.0
 	github.com/google/uuid v1.1.1
 	github.com/googleapis/gnostic v0.4.1
---
+++ test/instrumentation/levee/verify-govet-levee.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+
+# Copyright 2020 The Kubernetes Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# This script vets all *.go files by running `go vet`. It is equivalent to
+# `make vet`.
+# Usage: `hack/verify-govet.sh` or `make vet`.
+# Note: This script is a vestigial redirection. Please do not add "real" logic.
+
+set -o errexit
+set -o pipefail
+
+KUBE_ROOT=$(dirname "${BASH_SOURCE[0]}")/../../../
+BAZEL_OUT_DIR="$KUBE_ROOT/bazel-bin"
+
+# Warn future developers against trying to link this into an sh_test
+if [ -e "${TEST_BINARY}" ]; then
+  echo "Due to requiring build environment and artifacts, vet must be run via make, not Bazel."
+fi
+
+pushd ${KUBE_ROOT}
+  bazel build //test/instrumentation/levee:levee
+popd
+
+LEVEE_BIN=$(find ${BAZEL_OUT_DIR}/test/instrumentation/levee -name levee -type f)
+CONFIG_FILE=${KUBE_ROOT}/test/instrumentation/levee/testdata/levee-config.json
+
+# make vet runs on all packages when no targets are provided
+make vet WHAT="-vettool=${LEVEE_BIN} -config=${CONFIG_FILE}"
